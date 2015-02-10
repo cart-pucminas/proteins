2f3a1d85c7532bad23b99059142f4631edbeaa28
LarissaLeijoto <larissa.leijoto@gmail.com>
2015-02-10 08:15:05 -0200

    mudança de pasta

---
+++ src/svm.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#define Malloc(type,n) (type *)malloc((n)*sizeof(type))
+
+/*
+       float* readDatabase(); // ler a base de dados e carrega para a memoria primaria
+       float* normalize(float* dataBase)
+
+        /* genetic algorithm 
+
+       float* selectFeature(float* dataBaseNormalized, ); //seleciona da base de dados o conjunto de caracteristicas 
+       float* discreteCossineTransform(float* selectedData);
+       float supportVectorMachine(float* transformedBase, float C, float gamma)
+       
+       /* suppot vector machine
+       
+
+*/
+
+
+
+float svm(unsigned *label, float *data, unsigned width, unsigned height, float c, float gamma)
+{
+      
+
+        /* Cross validation. */
+        
+        /* Train. */
+        
+        /* Prediction. */
+
+  //      return (accuracy);
+}
